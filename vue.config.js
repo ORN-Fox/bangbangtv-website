e2b55fb07c96bb9e315d6e08ff335a44322c711b
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2024-09-21 12:09:44 -0400

    feat: import scss style

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/style.scss";
+        `
+      }
+    }
+  },
   transpileDependencies: true
 })
