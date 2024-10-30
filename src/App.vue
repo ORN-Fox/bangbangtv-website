e68f39001787408bf39414560f14819e53e8f80a
ORN_Fox <foxdog.orion@gmail.com>
2024-10-29 23:42:59 -0400

    chore: remove useless comments in app.vue

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,7 @@
 </template>
 
 <style lang="scss">
-// nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+// scss
 </style>
 
 <script lang="ts">
